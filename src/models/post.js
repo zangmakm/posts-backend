e7f623c185474f2fee4a5cd22065f3bdbcbab9a7
zangmakm <zangmakm@gmail.com>
2020-05-29 21:46:32 +1000

    add commnd

--- src/models/post.js
+++ src/models/post.js
@@ -47,3 +47,56 @@ module.exports = {
   getPostIndexById,
   doesIdExist,
 };
+
+// const posts = [];
+// let currentId = 1;
+
+// app.get("/posts", (req, res) => {
+//   return res.json(posts);
+// });
+
+// app.get("/posts/:id", (req, res) => {
+//   const { id } = req.params;
+//   const post = posts.find((i) => i.id === Number(id));
+//   if (!post) {
+//     return res.sendStatus(404);
+//   }
+//   res.json(post);
+// });
+
+// app.post("/posts", (req, res) => {
+//   const { author, content } = req.body;
+//   const newPost = { author, content, id: currentId++ };
+//   posts.push(newPost);
+//   res.json(newPost);
+// });
+
+// app.put("/posts/:id", (req, res) => {
+//   const { id } = req.params;
+//   const { author, content } = req.body;
+//   const post = posts.find((i) => i.id === Number(id));
+//   if (!post) {
+//     return res.sendStatus(404);
+//   }
+//   post.author = author;
+//   post.content = content;
+//   res.json(post);
+// });
+
+// app.delete("/posts/:id", (req, res) => {
+//   const { id } = req.params;
+//   //   let postIndex = -1;
+//   //   for (let i = 0; i < posts.length; i++) {
+//   //     if (posts[i].id === Number(id)) {
+//   //       postIndex = i;
+//   //       break;
+//   //     }
+//   //   }
+//   const postIndex = posts.findIndex((i) => i.id === Number(id)); // -1, 0 - (length-1)
+//   if (postIndex === -1) {
+//     return res.sendStatus(404);
+//   }
+//   const deletedPost = posts.splice(postIndex, 1);
+//   // res.sendStatus(200);
+//   res.json(deletedPost);
+// });
